64419ac8c5d3837b8998072e4dcc03a0a09794e5
Arpita Belsare <arpitabelsare22@navgurukul.org>
2024-12-10 13:57:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import First from './Component/First';
+
+const App = () => {
+  return (
+   <>
+   <First/>
+   </>
+  )
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Component/First.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+const First = () => {
+  return (
+   <>
+<div className='relative '>
+<div className='bg-blue-700 h-96 w-full py-14' >
+    <h1 className='text-9xl	text-white font-medium text-center font-sans tracking-wider'>ORTHOPEDIC</h1>
+    <div className='border-4 h-96 w-4/5 absolute top-36 bg-cyan-900 left-36 border-sky-200 rounded-xl py-6 px-6'>
+<nav className='bg-white w-full h-20'>
+<h2>RWJBARNABAS</h2>
+</nav> 
+</div>
+   </div>
+   <div className='bg-cyan-900 h-96 w-full'>
+
+   </div>
+</div>
+   </>
+
+  )
+}
+
+export default First;
+
+
+
